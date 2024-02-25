677cd73b041eeb1068e8bf65031741e109519397
Szymon Graczyk <s.graczyk@text.com>
2024-02-25 20:28:38 +0100

    Reorganize how BASE_URL is used and set

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
+    baseURL: process.env.BASE_URL,
     isMobile: true,
     // hasTouch: true,
 
