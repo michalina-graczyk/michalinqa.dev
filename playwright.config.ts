f3ba13aa831b48a528c18ad585887cf8d87fa7ab
Szymon Graczyk <s.graczyk@text.com>
2024-02-26 09:15:02 +0100

    Increase workers number for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 2 : 4,
   reporter: "html",
   use: {
     baseURL: process.env.BASE_URL || "http://localhost:3000",
