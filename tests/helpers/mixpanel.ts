682733de4c7253bf79f561a3e87c5fa9a7c3aec7
M.Graczyk <michalina.graczyk@saleor.io>
2024-12-08 22:39:44 +0100

    Add CV and Blog pages with updated navigation paths (#102)

--- tests/helpers/mixpanel.ts
+++ tests/helpers/mixpanel.ts
@@ -1,4 +1,5 @@
-import { Page, expect } from "@playwright/test";
+import { expect } from "@playwright/test";
+import type { Page } from "@playwright/test";
 import type { Dict } from "mixpanel-browser";
 
 export async function getTrackedEvents(page: Page) {
--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -17,14 +17,16 @@ test.describe("Page Navigation", () => {
       if (isMobile) {
         const menuIcon = page.locator("#astronav-menu");
         await expect(menuIcon).toBeVisible();
-        const menuItems = page.locator(".astronav-toggle");
-        await expect(menuItems).toHaveCount(3);
+        const menuItems = page.locator(
+          "nav li a[aria-label='navigation link']"
+        );
+        await expect(menuItems).toHaveCount(5);
       }
     });
   });
 
   test.describe("Desktop Navigation", () => {
-    test("Navigation header and items are correctly displayed and functional", async ({
+    test("Hash navigation works and tracks events", async ({
       page,
       isMobile,
       baseURL,
@@ -32,14 +34,15 @@ test.describe("Page Navigation", () => {
       const navigationHeader = page.locator('[data-testid="header"]');
       if (!isMobile) {
         await expect(navigationHeader).toBeVisible();
-        const navItems = [
-          { name: "O mnie", nav: "about" },
-          { name: "Oferta", nav: "offers" },
-          { name: "Kontakt", nav: "contact" },
+        const hashNavItems = [
+          { name: "O mnie", nav: "/#about" },
+          { name: "Oferta", nav: "/#offers" },
+          { name: "Kontakt", nav: "/#contact" },
         ];
-        for (const item of navItems) {
+
+        for (const item of hashNavItems) {
           await navigationHeader.locator(`:text("${item.name}")`).click();
-          await expect(page).toHaveURL(`${baseURL}#${item.nav}`);
+          await expect(page).toHaveURL(`${baseURL}${item.nav}`);
 
           const mixpanelEventsTracked = await getTrackedEvents(page);
           expectLastEventToBeTracked(
@@ -47,6 +50,26 @@ test.describe("Page Navigation", () => {
             "Menu item clicked",
             { Item: item.name }
           );
+        }
+      }
+    });
+
+    test("Page navigation works", async ({ page, isMobile, baseURL }) => {
+      const navigationHeader = page.locator('[data-testid="header"]');
+      if (!isMobile) {
+        await expect(navigationHeader).toBeVisible();
+        const pageNavItems = [
+          { name: "CV", nav: "/cv" },
+          { name: "Blog", nav: "/blog" },
+        ];
+
+        for (const item of pageNavItems) {
+          await navigationHeader.locator(`:text("${item.name}")`).click();
+          const expectedUrlPattern = new RegExp(
+            `${baseURL}${item.nav}/?$`,
+            "i"
+          );
+          await expect(page).toHaveURL(expectedUrlPattern);
         }
       }
     });
